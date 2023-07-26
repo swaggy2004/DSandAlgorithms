bfb33a047bd464b2dd93610a856202f92e1d81c0
Swaggy <101066701+swaggy2004@users.noreply.github.com>
2023-07-26 21:02:48 +0000

    This is the first code. A Basic sorting algorithm

---
+++ basicSort.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Initalizing the array
+    int array[] = { 3, 6, 7, 1, 4, 2, 8, 5 };
+
+    // Printing the unchanged array
+    printf("Unsorted: ");
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%d ", array[i]);
+    }
+    printf("\n");
+
+    // Sorting the array
+     for (int i = 0; i < 8; i++)
+     {
+        for (int j = i + 1; j < 8; j++)
+        {
+            // To sort in ascending order
+
+            // Checks if there are numbers after the ith position that are smaller. This logic can be flipped to get the descending order
+            if (array[j] < array[i])
+            {
+                int tmp = array[i];
+                array[i] = array[j];
+                array[j] = tmp;
+            }
+        }
+     }
+
+    printf("Sorted: ");
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%d ", array[i]);
+    }
+
+    return 0;
+}
